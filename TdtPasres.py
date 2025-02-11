5056857fbc3f3ff032b673e88a61dd9ba4e1b010
AleksI7 <a_h.79@mail.ru>
2025-02-11 15:04:20 +0700

    - добавлен парсер csv формата

--- TdtPasres.py
+++ TdtPasres.py
@@ -7,8 +7,9 @@ KEY_TIME_STAMP = 'TIME_STAMP'
 RE_LINE_NUM_PTRN = r'\d+'
 # 06.02.2025 13:52:25
 RE_DATE_PTRN = r'\d{2}.\d{2}.\d{4}'
-RE_TIME_PTRN = r'\d{2}:\d{2}:\d{2}'
-RE_DTTM_PTRN = RE_DATE_PTRN + '\s' + RE_TIME_PTRN
+RE_SECONDS = r':\d{2}'
+RE_TDT_TIME_PTRN = r'\d{2}:\d{2}' + RE_SECONDS
+RE_TDT_DTTM_PTRN = RE_DATE_PTRN + '\s' + RE_TDT_TIME_PTRN
 
 # ^1.1 25,4 X X 
 RE_TEMP_PREFIX = r'\s\^'
@@ -18,10 +19,20 @@ RE_TEMP_POSTFIX = r' X X'
 RE_SNSR_DATA_PTRN = RE_TEMP_PREFIX + RE_SNSR_NUM + r'\s' + RE_TEMP + RE_TEMP_POSTFIX
 RE_SNSRS_DATA =  r'(' + RE_SNSR_DATA_PTRN + r')+'
 
-# регулярка всей строки
-RE_DATA_LINE = RE_LINE_NUM_PTRN + r'\s' + RE_DTTM_PTRN + RE_SNSRS_DATA
+# регулярка всей строки tdt формата
+RE_TDT_DATA_LINE = RE_LINE_NUM_PTRN + r'\s' + RE_TDT_DTTM_PTRN + RE_SNSRS_DATA
 
-class Parser:
+
+# регулярка всей строки csv формата
+# 04.02.2025 18:21;24,2;23,7;25;25,3;25,6;24,4;23
+RE_CSV_SPLITTER = r';'
+RE_CSV_DTTM_PTRN = RE_DATE_PTRN + r'\s' + r'\d{2}:\d{2}'
+RE_CSV_TEMP_PTRN = r'\d+(,\d+)?'
+RE_CSV_SNSRS_DATA = r'(' + RE_CSV_SPLITTER + RE_CSV_TEMP_PTRN + r')+'
+RE_CSV_DATA_LINE = RE_CSV_DTTM_PTRN + RE_CSV_SNSRS_DATA
+
+
+class ParserTdt:
      
     def __init__(self):
         pass
@@ -43,7 +54,7 @@ class Parser:
         # print(parsed_lines)
 
     def LineValidate(self, line: str=''):
-        res = re.search(RE_DATA_LINE, line)
+        res = re.search(RE_TDT_DATA_LINE, line)
         if res:
             # print(res.group(0))
             return True
@@ -56,7 +67,7 @@ class Parser:
         return None
 
     def GetDateTimeFromLine(self, line: str=''):
-        res = re.search(RE_DTTM_PTRN, line)
+        res = re.search(RE_TDT_DTTM_PTRN, line)
         date_time_str = res.group(0)
         # 06.02.2025 13:53:19
         format = '%d.%m.%Y %H:%M:%S'
@@ -72,10 +83,69 @@ class Parser:
                 continue
             sensor_num = re.search(RE_SNSR_NUM, data_str).group(0)
             sensor_temp = re.search(RE_TEMP, data_str).group(0)
+            sensor_temp = sensor_temp.replace(',','.')
+            sensor_temp = float(sensor_temp)
             result[sensor_num] = sensor_temp
         return result
 
     def GetParsedDataAsJson(self):
+        result = ''
+        for parsed_line in self.parsed_lines:
+            result += json.dumps(parsed_line) + '\n'
+        return result
+
+class ParserCsv:
+     
+    def __init__(self):
+        pass
+
+    def RawDataParse(self, raw_data: str=''):
+        lines = raw_data.split('\n')
+        self.parsed_lines = []
+        for line in lines:
+            if not self.LineValidate(line):
+                continue
+            pass
+            date_time = self.GetDateTimeFromLine(line)
+            date_time_str = date_time.strftime('%m.%d.%YT%H:%M:%S')
+            timestamp = date_time.timestamp()
+            sensors_data = self.GetSensorsData(line)
+            sensors_data[KEY_TIME_STAMP] = timestamp
+            sensors_data['DATE_TIME'] = date_time_str
+            self.parsed_lines.append(sensors_data)
+
+    def LineValidate(self, line: str=''):
+        res = re.search(RE_CSV_DATA_LINE, line)
+        if res:
+            # print(res.group(0))
+            return True
+        return False
+
+    def GetDateTimeFromLine(self, line: str=''):
+        res = re.search(RE_CSV_DTTM_PTRN, line)
+        date_time_str = res.group(0)
+        # 06.02.2025 13:53:19
+        format = '%d.%m.%Y %H:%M'
+        date_time = datetime.strptime(date_time_str, format)
+        return date_time
+
+    def GetSensorsData(self, line: str=''):
+        sensors_data = re.search(RE_CSV_SNSRS_DATA, line)
+        split_data_str = re.split(RE_CSV_SPLITTER, sensors_data.group(0))
+        result = {}
+        sensor_num = 1
+        for data_str in split_data_str:
+            if data_str == '':
+                continue
+            # sensor_num = re.search(RE_SNSR_NUM, data_str).group(0)
+            sensor_temp = re.search(RE_CSV_TEMP_PTRN, data_str).group(0)
+            sensor_temp = sensor_temp.replace(',','.')
+            sensor_temp = float(sensor_temp)
+            result[sensor_num] = sensor_temp
+            sensor_num += 1
+        return result
+
+    def GetParsedDataAsJson(self): # TODO вынести в базовый класс
         result = ''
         for parsed_line in self.parsed_lines:
             result += json.dumps(parsed_line) + '\n'
@@ -92,10 +162,29 @@ test_raw_tdt_data = '1 06.02.2025 13:52:25 ^1.1 25,0 X X ^1.2 24,9 X X ^1.3 32,1
 9 06.02.2025 14:00:23 ^1.1 32,0 X X ^1.2 32,6 X X ^1.3 37,4 X X ^1.4 42,3 X X ^1.5 35,5 X X ^1.6 37,5 X X ^1.7 35,4 X X ^1.8 21,8 X X ^1.9 22,4 X X ^1.10 21,5 X X ^1.11 22,0 X X ^1.12 20,9 X X ^1.13 21,6 X X ^1.14 22,0 X X ^1.15 21,3 X X ^1.16 21,5 X X \n'
 
 
-def main():
-    parser = Parser()
+test_raw_csv_data = '06.02.2025 14:52;26;26,4;27,8;36;29,9;29,3;28,9;21,8\n\
+06.02.2025 14:53;26,3;26,3;27,8;35,9;29,8;29,4;28,9;21,8\n\
+06.02.2025 14:55;26,3;26,4;27,9;36,5;30,4;29,8;29,4;21,8\n\
+06.02.2025 14:57;25,5;25,7;29,1;37;30,8;31,8;36,1;21,7\n\
+06.02.2025 14:59;26,1;25,8;31,3;37,4;31,5;33,9;40,7;21,8\n\
+06.02.2025 15:01;29,3;30,2;33,8;38,1;32,4;36,4;44,3;21,8\n\
+06.02.2025 15:03;32,6;34,2;36,1;39,2;33,8;39,1;47,5;21,8\n\
+06.02.2025 15:04;34,1;35,5;37;39,6;34,4;39,9;48,7;21,8\n\
+06.02.2025 15:06;36,9;38,1;39,2;40,9;35,7;42,1;51,3;21,7\n\
+06.02.2025 15:08;38,9;40;41,1;42;37,1;44;53,3;21,8\n\
+06.02.2025 15:10;42,9;42,6;42,6;43,4;39,8;42,4;51,9;21,7\n\
+06.02.2025 15:12;44;43,7;43,1;44,6;41,6;41,4;50,8;21,6\n\
+06.02.2025 15:14;43,9;43,5;43,2;45,4;42,7;41,3;55,3;21,8\n'
 
-    parser.RawDataParse(test_raw_tdt_data)
+def main():
+    tdt_parser = ParserTdt()
+    tdt_parser.RawDataParse(test_raw_tdt_data)
+
+    csv_parser = ParserCsv()
+    csv_parser.RawDataParse(test_raw_csv_data)
+    json_data = csv_parser.GetParsedDataAsJson()
+
+    pass
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import re
-from TdtPasres import Parser
+from TdtPasres import ParserTdt
+from TdtPasres import ParserCsv
 
-FILE_NAME = 'Arch Термодат-29 v5.11 04.02.2025-07.02.2025.tdt'
 
-def main():
-    file_name = FILE_NAME
+FILE_NAME_TDT = 'Arch Термодат-29 v5.11 04.02.2025-07.02.2025.tdt'
+FILE_NAME_CSV = '06-07.02.2025.csv'
+
+def ParseTdtFile(file_name: str=''):
     content = ''
     try:
         f = open(file_name, 'rt')
@@ -16,7 +18,7 @@ def main():
         print(f'Невозможно открыть файл: {file_name}')
         return
 
-    parser = Parser()
+    parser = ParserTdt()
     parser.RawDataParse(content)
     json_data = parser.GetParsedDataAsJson()
 
@@ -28,6 +30,31 @@ def main():
     f.write(json_data)
     f.close()
 
+def ParseCsvFile(file_name: str=''):
+    content = ''
+    try:
+        f = open(file_name, 'rt')
+        content =  f.read()
+        f.close()
+    except:
+        print(f'Невозможно открыть файл: {file_name}')
+        return
+
+    parser = ParserCsv()
+    parser.RawDataParse(content)
+    json_data = parser.GetParsedDataAsJson()
+
+    split_name = file_name.split('.')
+    ext = split_name[len(split_name)-1]
+    out_file = file_name.replace(ext, 'json')
+
+    f = open(out_file, 'wt')
+    f.write(json_data)
+    f.close()
+
+def main():
+    ParseTdtFile(FILE_NAME_TDT)
+    ParseCsvFile(FILE_NAME_CSV)
     pass
 
 if __name__ == '__main__':
